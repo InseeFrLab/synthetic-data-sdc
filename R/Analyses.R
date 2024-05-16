84e08caaa4129835c91c5246096849591c6332eb
julienhelfenstein <julien.helfenstein@insee.fr>
2024-05-16 14:22:01 +0000

    analyses

--- R/Analyses.R
+++ R/Analyses.R
@@ -8,6 +8,7 @@ library(tictoc)
 library(ggplot2)
 library(hrbrthemes)
 library(viridis)
+library(abind)
 
 # Import des données -----------------
 FILE_KEY_IN_S3 <- "20240512_sim_synthpop_sample_cart_ctree_parametric_bag_rf_500_sims.RDS"
@@ -23,9 +24,6 @@ res_simul <- aws.s3::s3read_using(
 str(res_simul, max.level=1)
 methodes <- which(names(res_simul) != "original")
 
-num = c("age", "depress", "nofriend", "height", "weight", "bmi")
-fac = c("sex", "agegr", "placesize", "region", "edu", "eduspec", "socprof", "marital", "ls", "trust", "trustfam", "trustneigh", "sport", "smoke", "alcsol", "wkabint", "englang")
-
 mes_modeles <- c("sample", "cart", "ctree", "parametric", "bag", "rf")
 
 n_sim <- 500
@@ -35,16 +33,17 @@ options(max.print = 10000)
 # Statistiques
 res_simul_empile <- map(res_simul[methodes], \(df_list) df_list %>% imap(\(df, i) df %>% mutate(index_sim = i)) %>% bind_rows())
 
-calc_stats_num <- function(df) {
+calc_stats_mean_num <- function(df) {
   df %>% group_by(index_sim) %>%
     summarise(
       across(where(is.numeric), ~ mean(.)
-      , .names = "{.col}"), .groups = "drop"
+             , .names = "{.col}"), .groups = "drop"
     ) %>% summarise(across(-index_sim, list(
       min = ~ min(.),
       max = ~ max(.),
       mean = ~ mean(.),
       sd = ~ sd(.),
+      cv = ~ sd(.) / mean(.),
       median = ~ median(.),
       q1 = ~ quantile(., 0.25),
       q3 = ~ quantile(., 0.75),
@@ -52,61 +51,122 @@ calc_stats_num <- function(df) {
       q975 = ~ quantile(., 0.975))))
 } 
 
-table_stats = imap(res_simul_empile, \(df, methode) calc_stats_num(df) %>% mutate(methode = methode)) %>% list_rbind() 
+table_mean_num <- imap(res_simul_empile, \(df, methode) calc_stats_mean_num(df) %>% mutate(methode = methode)) %>% list_rbind() 
 
-# Calcul des statistiques
-stats_all_meth <- imap(
-  res_simul[methodes], 
-  \(res_one_methode, nom_methode) {
-    plan(multisession, workers = 10)
-    
-    res <- future_map(
-      res_one_methode, 
-      calc_stats, 
-      .progress = TRUE
-    ) %>% list_rbind() %>% summarise(across(everything(), mean))
-    
-    plan(sequential)
-    
-    res <- res %>%
-      mutate(method = nom_methode)
-    
-    return(res)
-  },
-  .progress = TRUE
-) %>% list_rbind()
 
-print(stats_all_meth)
+calc_org_mean_num <- function(df) {
+  df %>% summarise(
+      across(where(is.numeric), list(
+      min = ~ min(.),
+      max = ~ max(.),
+      mean = ~ mean(.),
+      sd = ~ sd(.),
+      cv = ~ sd(.) / mean(.),
+      median = ~ median(.),
+      q1 = ~ quantile(., 0.25),
+      q3 = ~ quantile(., 0.75),
+      q025 = ~ quantile(., 0.025),
+      q975 = ~ quantile(., 0.975)
+    )))
+}
 
-stats_moy = c("min", "max", "mean", "median", "sd", "q1", "q3", "q025", "q975")
+table_org_mean_num <- calc_stats_mean_num(res_simul$original)
 
-for (i in 1:length(mes_modeles)) {
-  for (j in 1:length(stats_moy)) {
-    stat = 0
-    for (k in seq(j, length(stats_all_meth) - 1, 9)) {
-      stat = stat + stats_all_meth[i, k]
+calc_stats_sd_num <- function(df) {
+  df %>% group_by(index_sim) %>%
+    summarise(
+      across(where(is.numeric), ~ sd(.)
+             , .names = "{.col}"), .groups = "drop"
+    ) %>% summarise(across(-index_sim, list(
+      min = ~ min(.),
+      max = ~ max(.),
+      mean = ~ mean(.),
+      sd = ~ sd(.),
+      cv = ~ sd(.) / mean(.),
+      median = ~ median(.),
+      q1 = ~ quantile(., 0.25),
+      q3 = ~ quantile(., 0.75),
+      q025 = ~ quantile(., 0.025),
+      q975 = ~ quantile(., 0.975))))
+}
+
+table_sd_num <- imap(res_simul_empile, \(df, methode) calc_stats_sd_num(df) %>% mutate(methode = methode)) %>% list_rbind() 
+
+
+calc_stats_cat <- function(df) {
+  cat_stats <- df %>%
+    select(index_sim, where(is.factor)) %>%
+    pivot_longer(cols = -index_sim, names_to = "variable", values_to = "modalites") %>%
+    group_by(index_sim, variable, modalites) %>%
+    summarise(freq = n(), .groups = "drop") %>%
+    group_by(variable, modalites) %>%
+    summarise(across(freq, list(
+      min = ~ min(.),
+      max = ~ max(.),
+      mean = ~ mean(.),
+      sd = ~ sd(.),
+      cv = ~ sd(.) / mean(.),
+      median = ~ median(.),
+      q1 = ~ quantile(., 0.25),
+      q3 = ~ quantile(., 0.75),
+      q025 = ~ quantile(., 0.025),
+      q975 = ~ quantile(., 0.975))))
+}
+
+table_cat <- imap(res_simul_empile, \(df, methode) calc_stats_cat(df) %>% mutate(methode = methode)) %>% list_rbind() 
+
+
+calc_correlation <- function(df) {
+  df %>%
+    select(where(is.numeric)) %>%
+    cor(use = "complete.obs")
+}
+
+table_cor <- map(
+  res_simul[methodes],
+  \(list_df){
+    array_cor <- map(
+      list_df,
+      \(df){
+        calc_correlation(df)
+      }
+    ) %>% abind::abind(along = 3) 
+    list(mean = array_cor %>% apply(MARGIN = 1:2, FUN = mean),
+         sd = array_cor %>% apply(MARGIN = 1:2, FUN = sd))
+  }
+)
+
+
+calc_mae_num <- function(df_syn, df_orig) {
+  df_syn %>% select(where(is.numeric))
+  df_orig %>% select(where(is.numeric))
+}
+
+table_mae_num <- imap(res_simul[methodes], \(list_df, methode) {
+  map_dfr(list_df, \(df_synth, i) {
+    df_orig <- res_simul$original[[i]]
+    calc_mae_num(df_synth, df_orig)
+  }) %>% mutate(methode = methode)
+}) %>% bind_rows()
+
+
+calc_mae <- function(liste_res, )
+for (i in 1:length(res_simul[methodes])) {
+  MSE = 0
+  for (j in 1:length(res_simul[[i]])) {
+    for (k in 1:length(res_simul[[i]][[j]])) {
+      MSE = MSE + mean(abs(res_simul[[i]][[j]][, k] - res_simul$original[, k]))
     }
-    stats_all_meth[i, length(stats_all_meth) + j]
   }
 }
 
 
-moy_moy = 0
-for (i in seq(3,length(stats_all_meth)-1,9)) {
-  moy_moy = moy_moy + stats_all_meth[1, i]
-}
 
 
 
-,
-across(where(is.factor), list(
-  min_n = ~ min(table(.)),
-  max_n = ~ max(table(.)),
-  mean_n = ~ mean(table(.)),
-  sd_n = ~ sd(table(.)),
-  median_n = ~ median(table(.)),
-  q1_n = ~ quantile(table(.), 0.25),
-  q3_n = ~ quantile(table(.), 0.75),
-  q025_n = ~ quantile(table(.), 0.025),
-  q975_n = ~ quantile(table(.), 0.975)
-), .names = "{.col}_{.fn}")
+
+
+
+
+
+
